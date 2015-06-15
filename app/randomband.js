64ad3c26ca507a1fabc62c4d5fa561dd8fac202f
Mark Buchthal <mark.buchthal@gmail.com>
2015-06-15 11:44:08 -0700

    add libs

--- app/randomband.js
+++ app/randomband.js
@@ -1,32 +1,47 @@
 $(function() {
 
-  $("form").on('submit', function (event) {
+  $("#submitWordAdjective").on('submit', function (event) {
     event.preventDefault();
     var userAdjective = $("#user-adjective").val();
     $.post("/adjective", {word: userAdjective}, function() {
-      $("#adjective-res").html(response.message + " <em>" + response.confirm + "</em">);
+      var confirm = response.message + " <em>" + response.confirm + "</em>";
+      $("#res-adjective").html(confirm);
+    });
+  });
+
+  $("#submitWordNoun").on('submit', function (event) {
+    event.preventDefault();
+    var userNoun = $("#user-noun").val();
+    $.post("/noun", {word: userNoun}, function() {
+      var confirm = response.message + " <em>" + response.confirm + "</em>";
+      $("#res-noun").html(confirm);
+    });
+  });
+
+  $("#submitWordVerb").on('submit', function (event) {
+    event.preventDefault();
+    var userVerb = $("#user-verb").val();
+    $.post("/verb", {word: userVerb}, function() {
+      var confirm = response.message + " <em>" + response.confirm + "</em>";
+      $("#res-verb").html(confirm);
     })
   });
 
   $('#buttonone').on('click', function() {
-    $.get("/adjective", function(response) {
+    $.get("/adjective", function (response) {
       $('#adjective').html(response.word);
     });
   })
 
-
-
   $('#buttontwo').on('click', function() {
-    $.get("/verb", function(response) {
-      $('#verb').html(response.word);
+    $.get("/noun", function (response) {
+      $('#noun').html(response.word);
     });
   })
 
-
-
   $('#buttonthree').on('click', function() {
-    $.get("/noun", function(response) {
-      $('#noun').html(response.word);
+    $.get("/verb", function (response) {
+      $('#verb').html(response.word);
     });
   })
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var getRandomWord = require('./lib/getRandomWord');
-var Adjective = require('./lib/adjective');
+var adjective = require('./lib/adjective');
+var noun = require('./lib/noun');
+var verb = require('./lib/verb');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,25 +11,6 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/app/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-var bandNames = [
-  "Wicked Purple Mountain Ranges",
-  "Pickled Princes",
-  "Neverly Appointed"
-];
-
-var adjectives = [
-  'pretty',
-  'ugly',
-  'wicked',
-  'foxy',
-  'witty',
-  'red',
-  'sick',
-  'savory'
-];
-
-var quotes = ["I like dogs.", "I hate cats.", "Death to penguins.", "Birds are noisy."];
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/app/index.html');
@@ -37,40 +20,36 @@ app.listen(port, function() {
   console.log('Server started on port ' + port);
 });
 
-// app.get("/", function (req, res) {
-//   var randomIndex = Math.floor(Math.random()*bandNames.length);
-//   res.send(bandNames[randomIndex]);
+// app.get("/quote", function (req, res) {
+//   var randomIndex = Math.floor(Math.random()*quotes.length);
+//   res.send(quotes[randomIndex]);  // returning text
 // });
 
-app.get("/quote", function (req, res) {
-  var randomIndex = Math.floor(Math.random()*quotes.length);
-  res.send(quotes[randomIndex]);  // returning text
-});
-
 app.post("/adjective", function (req, res) {
+  adjective[req.body.word] = true;
   res.json( postWord(req.body.word, adjective));
 });
 
 app.post("/noun", function (req, res) {
   noun[req.body.word] = true;
+  res.json( postWord(req.body.word, noun));
 });
 
 app.post("/verb", function (req, res) {
   verb[req.body.word] = true;
+  res.json( postWord(req.body.word, verb));
 })
 
 app.get("/adjective", function (req, res) {
-  var randomIndex = Math.floor(Math.random()*adjectives.length);
-  res.json( { "word": adjectives[randomIndex] });  // returning a JSON objective
+  res.json(getRandomWord(adjective));  // returning a JSON objective
 });
 
 app.get("/noun", function (req, res) {
-  var randomIndex = Math.floor(Math.random()*nouns.length);
-  res.json( { "word": nouns[randomIndex] });
+  res.json(getRandomWord(noun));
 });
 
 app.get("/verb", function (req, res) {
-  var randomIndex = Math.floor(Math.random()*verbs.length);
+  res.json(getRandomWord(verb));
 });
 
 function postWord (word, wordObject) {
@@ -81,4 +60,6 @@ function postWord (word, wordObject) {
     wordObject[word] = true;
     msg = "We saved the awesome word: ";
   return { message: msg, confirm: word };
-}
+  }
+};
+
